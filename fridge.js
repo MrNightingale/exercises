a28538266ff5239ffba84fd8da79c57476ee4755
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2015-07-23 23:32:31 +0300

    I'v added  Machine (Parent) and Fridge (Child) Classes

--- fridge.js
+++ fridge.js
@@ -1,7 +1,7 @@
 /**
  * Created by Solovyov on 23.07.2015.
  */
-function Machine() {
+function Machine(power) {
     this._power = power;
     this._enabled = false;
 
@@ -16,3 +16,34 @@ function Machine() {
     };
 }
 
+function Fridge(power) {
+    Machine.apply(this, arguments);
+
+    var food = [];
+
+    this.addFood = function(item) {
+        if (!this._enabled) {
+            throw new Error('Fridge is not working. Please, enable it.');
+        }
+        else if (food.length >= (this._power / 100)) {
+            throw new Error('So much food. Not enough power.');
+        }
+        else {
+            food.push(item);
+        }
+    };
+
+    this.getFood = function() {
+        return food;
+    };
+}
+
+var fridge = new Fridge(500);
+
+fridge.enable();
+fridge.addFood('meat', 'eggs');
+
+var fridgeFood = fridge.getFood();
+alert( fridgeFood );
+
+fridge.disable();
