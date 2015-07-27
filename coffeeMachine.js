70b90e2ffa0dd039d929ab073e178b9f82e9d3c1
Vladyslav Soloviov <soloviov.v.v@gmail.com>
2015-07-27 23:02:29 +0300

    Now you can add to the "fridge" not only one item per one use.

---
+++ coffeeMachine.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Solovyov on 27.07.2015.
+ */
+function CoffeeMachine(power) {
+    this._power = power;
+    this._waterAmount = 0;
+}
+
+CoffeeMachine.prototype.WATER_HEAT_CAPACITY = 4200;
+
+CoffeeMachine.prototype._getTimeToBoil = function() {
+    return this._waterAmount * this.WATER_HEAT_CAPACITY * 80 / this._power;
+};
+
+CoffeeMachine.prototype.run = function() {
+    setTimeout(function() {
+        alert('Coffee is ready!');
+    }, this._getTimeToBoil());
+};
+
+CoffeeMachine.prototype.setWaterAmount = function(amount) {
+    this._waterAmount = amount;
+};
+
+var coffeeMachine = new CoffeeMachine(10000);
+coffeeMachine.setWaterAmount(50);
+coffeeMachine.run();
